ab1af3a07d81117455bd3dbeaaaf0954c560e8ec
KarimSekri <karim.sekri@gmail.com>
2023-11-15 03:36:53 +0100

    Affichage des musiques en cours

--- src/components/Musiques.tsx
+++ src/components/Musiques.tsx
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Musiques = () => {
-    
+    const [musiques, setMusiques] = useState([]);
+
 
     useEffect(() => {
         const getMusiquesApi = async () => {
             const reponse = await fetch(`http://localhost:1337/api/musiques`);
             const musiques = await reponse.json();
             console.log("musiques", musiques.data);
+            setMusiques(musiques.data);
         }
         getMusiquesApi()
     }, [])
@@ -15,6 +17,11 @@ const Musiques = () => {
     return (
         <>
             <h1>Musiques</h1>
+            {musiques.map((valeur, index) => (
+                
+                <div key = {index}> {JSON.stringify(valeur.attributes.Titre)}</div>
+                   
+            ))}
         </>
     )
 }
